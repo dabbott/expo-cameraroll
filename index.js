72fd6e752b9a4cd762ac84f669f17c1af51099f6
Devin Abbott <devinabbott@gmail.com>
2020-10-26 11:10:40 -0700

    Use MediaLibrary constants to support Expo SDK 36-39

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import * as MediaLibrary from "expo-media-library";
+import * as MediaLibrary from 'expo-media-library'
+
+const ASSET_TYPE_TO_MEDIA_TYPE = {
+  Photos: MediaLibrary.MediaType.photo,
+  Videos: MediaLibrary.MediaType.video,
+}
 
 /**
  * `CameraRoll` provides access to the local camera roll or photo library.
@@ -15,23 +20,23 @@ export default {
    * Returns a Promise with photo identifier objects from the local camera
    * roll of the device matching shape defined by `getPhotosReturnChecker`.
    *
-   * See https://facebook.github.io/react-native/docs/cameraroll.html#getphotos
+   * See https://reactnative.dev/docs/cameraroll.html#getphotos
    */
   async getPhotos(params) {
     const mediaLibraryParams = {
       first: params.first,
       after: params.after,
-      mediaType: params.assetType
-    };
+      mediaType: ASSET_TYPE_TO_MEDIA_TYPE[params.assetType] || params.assetType,
+    }
 
-    const results = await MediaLibrary.getAssetsAsync(mediaLibraryParams);
+    const results = await MediaLibrary.getAssetsAsync(mediaLibraryParams)
 
-    results.assets.forEach(asset => {
-      this._store[asset.uri] = asset;
-    });
+    results.assets.forEach((asset) => {
+      this._store[asset.uri] = asset
+    })
 
     const transformedResults = {
-      edges: results.assets.map(asset => ({
+      edges: results.assets.map((asset) => ({
         node: {
           type: asset.mediaType,
           image: {
@@ -39,20 +44,20 @@ export default {
             uri: asset.uri,
             height: asset.height,
             width: asset.width,
-            playableDuration: asset.duration
+            playableDuration: asset.duration,
           },
           timestamp: asset.modificationTime,
-          location: null
-        }
+          location: null,
+        },
       })),
       page_info: {
         has_next_page: results.hasNextPage,
         start_cursor: null,
-        end_cursor: results.endCursor
-      }
-    };
+        end_cursor: results.endCursor,
+      },
+    }
 
-    return transformedResults;
+    return transformedResults
   },
 
   /**
@@ -60,14 +65,14 @@ export default {
    * On Android: Deletes a set of photos from the camera roll.
    */
   async deletePhotos(photoUris) {
-    const assets = photoUris.map(uri => this._store[uri]).filter(x => !!x);
+    const assets = photoUris.map((uri) => this._store[uri]).filter((x) => !!x)
 
-    const ok = await MediaLibrary.deleteAssetsAsync(assets);
+    const ok = await MediaLibrary.deleteAssetsAsync(assets)
 
     if (ok) {
-      photoUris.forEach(uri => {
-        delete this._store[uri];
-      });
+      photoUris.forEach((uri) => {
+        delete this._store[uri]
+      })
     }
   },
 
@@ -75,19 +80,19 @@ export default {
    * Saves the photo or video to the camera roll or photo library.
    */
   async save(tag, options = {}) {
-    const asset = await MediaLibrary.createAssetAsync(tag);
+    const asset = await MediaLibrary.createAssetAsync(tag)
 
     if (options.album) {
-      await MediaLibrary.addAssetsToAlbumAsync([asset], options.album, false);
+      await MediaLibrary.addAssetsToAlbumAsync([asset], options.album, false)
     }
 
-    return asset.uri;
+    return asset.uri
   },
 
   /**
    * Saves the photo or video to the camera roll or photo library.
    */
   async saveToCameraRoll(tag, type) {
-    return this.save(tag, { type });
-  }
-};
+    return this.save(tag, { type })
+  },
+}
